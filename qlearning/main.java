01f31037d625bdb72c5035c085ac5adf87028395
Romeorubiko <raulescabia255@gmail.com>
2018-04-23 16:12:20 +0200

    codigo actualizado al mapa

--- qlearning/main.java
+++ qlearning/main.java
@@ -14,62 +14,67 @@ import java.util.*;
  */
 public class main 
 {
+	public static final int DERECHA=0;
+	public static final int IZQUIERDA=1;
+	public static final int ARRIBA=2;
+	public static final int ABAJO=3;
+	
     public static void main(String[] args) 
     {   
         List<Tupla> mapa  = new ArrayList<Tupla>();
-        
+        		
         String[] acciones = {"0", "1", "2", "3"};
-        String[] estados  = {"0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15"};
+        String[] estados  = {"0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18","19"};
         
         double[] estado = {2};
-        double[] estadoFinal = {13};
+        double[] estadoFinal = {17};
         double[] accion = {0};
         double refuerzo = 0;
         
         boolean encontrado;
         
-        QLearning ql        = new QLearning(0, 0.6, 0.3, estados, acciones, 16, 4);
+        QLearning ql        = new QLearning(0, 0.6, 0.3, estados, acciones, 20, 4);
         int ciclosMaximos   = 5;
         int ciclos          = 0;
         int posicion;
 		
-		mapa.add(new Tupla(0, 0, 1, 1.25)); 
-        mapa.add(new Tupla(0, 1, 5, 1.25)); 
-        mapa.add(new Tupla(1, 0, 2, 1.3)); 
-        mapa.add(new Tupla(1, 1, 6, 1.3)); 
-        mapa.add(new Tupla(1, 2, 0, 0.8)); 
-        mapa.add(new Tupla(2, 0, 3, 0.75)); 
-        mapa.add(new Tupla(2, 2, 1, 0.75)); 
-        mapa.add(new Tupla(3, 0, 4, 0.8)); 
-        mapa.add(new Tupla(3, 1, 7, 1.3)); 
-        mapa.add(new Tupla(3, 2, 2, 1.3)); 
-        mapa.add(new Tupla(4, 2, 3, 1.25)); 
-        mapa.add(new Tupla(5, 0, 6, 1.3)); 
-        mapa.add(new Tupla(5, 1, 8, 1.3)); 
-        mapa.add(new Tupla(5, 3, 0, 0.8)); 
-        mapa.add(new Tupla(6, 2, 5, 0.75)); 
-        mapa.add(new Tupla(6, 3, 1, 0.75)); 
-        mapa.add(new Tupla(7, 1, 9, 1.5)); 
-        mapa.add(new Tupla(7, 3, 3, 0.75)); 
-        mapa.add(new Tupla(8, 1, 11, 1.5)); 
-        mapa.add(new Tupla(8, 3, 5, 0.75)); 
-        mapa.add(new Tupla(9, 0, 10, 0.6)); 
-        mapa.add(new Tupla(9, 1, 14, 2)); 
-        mapa.add(new Tupla(9, 3, 7, 0.6)); 
-        mapa.add(new Tupla(10, 1, 15, 1.5));
-        mapa.add(new Tupla(10, 2, 9, 1.5));
-        mapa.add(new Tupla(11, 0, 12, 2));
-        mapa.add(new Tupla(11, 3, 8, 0.6));
-        mapa.add(new Tupla(12, 0, 13, 4));
-        mapa.add(new Tupla(12, 2, 11, 0.5));
-        mapa.add(new Tupla(13, 0, 14, 0.1));
-        mapa.add(new Tupla(13, 2, 12, 0.1));
-        mapa.add(new Tupla(14, 0, 15, 0.5));
-        mapa.add(new Tupla(14, 2, 13, 4));
-        mapa.add(new Tupla(14, 3, 9, 0.5));
-        mapa.add(new Tupla(15, 2, 14, 2));
-        mapa.add(new Tupla(15, 3, 10, 0.6));
-
+		//estadoActual,acción, estadoSiguiente, refuerzo
+		mapa.add(new Tupla(0, DERECHA, 1, 0)); 
+        mapa.add(new Tupla(0, ABAJO, 5, 0)); 
+		mapa.add(new Tupla(1, IZQUIERDA, 0, 0));
+		mapa.add(new Tupla(1, DERECHA, 2, 0));
+		mapa.add(new Tupla(2, IZQUIERDA, 1, 0));
+		mapa.add(new Tupla(2, ABAJO, 3, 0));
+		mapa.add(new Tupla(3, IZQUIERDA, 2, 0));
+		mapa.add(new Tupla(3, DERECHA, 4, 0));
+		mapa.add(new Tupla(3, ABAJO, 8, 0));
+		mapa.add(new Tupla(4, IZQUIERDA, 3, 0));
+		mapa.add(new Tupla(5, ARRIBA, 0, 0));
+		mapa.add(new Tupla(5, ABAJO, 10, 0));
+		mapa.add(new Tupla(8, ARRIBA, 3, 0));
+		mapa.add(new Tupla(8, ABAJO, 13, 0));
+		mapa.add(new Tupla(10, ARRIBA, 5, 0));
+		mapa.add(new Tupla(10, DERECHA, 10, 0));
+		mapa.add(new Tupla(11, ABAJO, 16, 0));
+		mapa.add(new Tupla(11, IZQUIERDA, 10, 0));
+		mapa.add(new Tupla(13, ARRIBA, 8, 0));
+		mapa.add(new Tupla(13, DERECHA, 14, 0));
+		mapa.add(new Tupla(13, ABAJO, 18, 0));
+		mapa.add(new Tupla(14, ABAJO, 19, 0));
+		mapa.add(new Tupla(14, IZQUIERDA, 13, 0));
+		mapa.add(new Tupla(16, ARRIBA, 11, 0));
+		mapa.add(new Tupla(16, DERECHA, 17, 1));
+		mapa.add(new Tupla(17, IZQUIERDA, 16, 0));
+		mapa.add(new Tupla(17, DERECHA, 18, 0));
+		mapa.add(new Tupla(18, ARRIBA, 13, 0));
+		mapa.add(new Tupla(18, DERECHA, 19, 0));
+		mapa.add(new Tupla(18, IZQUIERDA, 17, 1));
+		mapa.add(new Tupla(19, ARRIBA, 14, 0));
+		mapa.add(new Tupla(19, IZQUIERDA, 18, 0));
+/*
+		mapa.add(new Tupla(18, DERECHA, 19, 0));
+		mapa.add(new Tupla(18, DERECHA, 19, 0));
+*/
         while (ciclos < ciclosMaximos)
         {
             for (int i = 0; i < mapa.size(); i++)
